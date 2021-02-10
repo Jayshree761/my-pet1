062de092b15f93a7ad322dc58fdee8b224d3cb13
Jayshree761 <67451717+Jayshree761@users.noreply.github.com>
2021-02-10 10:46:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//Create variables here
+var dogImg, happyDog, database,foodS , foodstock,dog;
+function preload()
+{
+  //load images here
+  dogImg= loadImage("images/d1.png");
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
